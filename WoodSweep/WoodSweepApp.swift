04476c063a520640d00dfb55d395438a44030f0e
hydazz <alexanderhyde@icloud.com>
2026-05-21 18:06:40 +1000

    Initial Commit

---
+++ WoodSweep/WoodSweepApp.swift
@@ -0,0 +1,17 @@
+//
+//  WoodSweepApp.swift
+//  WoodSweep
+//
+//  Created by Alexander Hyde on 21/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct WoodSweepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
